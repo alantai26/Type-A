6cc2047e008910418e8dd76b08bd72849b2ba3da
alantai26 <a.tai@northeastern.edu>
2026-04-29 22:52:58 -0400

    TYP-17 add auth stuff

--- backend/alembic/versions/9a5cecfca606_rename_outing_and_event_draft_status_to_.py
+++ backend/alembic/versions/9a5cecfca606_rename_outing_and_event_draft_status_to_.py
@@ -18,35 +18,11 @@ branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
-def upgrade() -> None:                                                                                                                  
-      """Upgrade schema."""                                                                                                               
-      op.execute("UPDATE outings SET status = 'planning_in_progress' WHERE status = 'draft'")
-      op.execute("UPDATE events  SET status = 'planning_in_progress' WHERE status = 'draft'")                                             
-                                                                                                                                          
-      op.drop_constraint('check_outing_status', 'outings', type_='check')                                                                 
-      op.create_check_constraint(                                                                                                         
-          'check_outing_status', 'outings',                                                                                               
-          "status IN ('planning_in_progress', 'confirmed', 'completed', 'cancelled')"
-      )                                                                                                                                   
-                  
-      op.drop_constraint('check_event_status', 'events', type_='check')                                                                   
-      op.create_check_constraint(
-          'check_event_status', 'events',                                                                                                 
-          "status IN ('planning_in_progress', 'confirmed', 'completed', 'cancelled')"
-      )           
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
 
 
 def downgrade() -> None:
-      """Downgrade schema."""
-      op.drop_constraint('check_outing_status', 'outings', type_='check')
-      op.create_check_constraint(                                                                                                         
-          'check_outing_status', 'outings',
-          "status IN ('draft', 'confirmed', 'completed', 'cancelled')"                                                                    
-      )                                                                                                                                   
-      op.drop_constraint('check_event_status', 'events', type_='check')
-      op.create_check_constraint(                                                                                                         
-          'check_event_status', 'events',                                                                                                 
-          "status IN ('draft', 'confirmed', 'completed', 'cancelled')"
-      )                                                                                                                                   
-      op.execute("UPDATE outings SET status = 'draft' WHERE status = 'planning_in_progress'")
-      op.execute("UPDATE events  SET status = 'draft' WHERE status = 'planning_in_progress'")   
+    """Downgrade schema."""
+    pass
